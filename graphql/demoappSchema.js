5f4dba6f60c7c454507711ac24bdcf62fa43aeae
bffconsoleapp <saraljankari@gmail.com>
2025-04-21 06:10:59 +0000

    Initial commit with schema and resolver

---
+++ graphql/demoappSchema.js
@@ -0,0 +1,62 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = `
+  type Credit_Union {
+    id: ID!
+    Contract_Number: String!
+    Credit_Union_Name: String!
+    premium_reports: [Premium_Report!]!
+    premium_adjustments: [Premium_Adjustment!]!
+    single_premium_certificate_returns: [Single_Premium_Certificate_Return!]!
+  }
+
+  type Premium_Report {
+    id: ID!
+    # Define additional fields if needed
+  }
+
+  type Premium_Adjustment {
+    id: ID!
+    Product_Name: String!
+    Report_Period: String!
+    Status: String!
+    Last_Update: String!
+    Period_Ending: String!
+    Adjustment_Type_to_the_Credit_Union: String!
+    Comment: String!
+    Total_Borrower_Fees_: Float!
+    CU_Retail_Rate: Float!
+    Protected_Loan_Amount: Float!
+    Pay_Rate: Float!
+    Premium_Due: Float!
+    Total_Amount: Float!
+  }
+
+  type Single_Premium_Certificate_Return {
+    id: ID!
+    # Define additional fields if needed
+  }
+
+  input EditPremiumAdjustmentInput {
+    id: ID!
+    Total_Borrower_Fees: Float!
+    CU_Retail_Rate: Float!
+    Protected_Loan_Amount: Float!
+    Pay_Rate: Float!
+    Premium_Due: Float!
+    Total_Amount: Float!
+  }
+
+  type Query {
+    searchCreditUnionByContractNumber(contractNumber: String!): [Credit_Union!]!
+    searchCreditUnionByName(name: String!): [Credit_Union!]!
+    searchCreditUnionByState(state: String!): [Credit_Union!]!
+    searchCreditUnionByNameAndState(name: String!, state: String!): [Credit_Union!]!
+  }
+
+  type Mutation {
+    editPremiumAdjustment(input: EditPremiumAdjustmentInput!): Premium_Adjustment!
+  }
+`;
+
+export default typeDefs;
--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import routes from './routes/app.route.js'; // Importing routes
 import { ApolloServer } from 'apollo-server-express'; // Import from apollo-server-express
 
 // Import GraphQL type definitions and resolvers
-import demotypeDefs from './graphql/demo.js';
-import demoresolver from './graphql/demoresolver.js';
+import demotypeDefs from './graphql/demoappSchema.js';
+import demoresolver from './graphql/demoappResolver.js';
 
 const app = express();
 app.use(express.json());
